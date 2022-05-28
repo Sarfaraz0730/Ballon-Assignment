0c8aac78af1e9626185e6156e9cca10d76681d5d
=Sarfaraz0730 <72138410+Sarfaraz0730@users.noreply.github.com>
2022-05-28 16:26:33 +0530

    started

---
+++ syngenta-baloon/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Balloon from './Components/Balloon';
+
+function App() {
+  return (
+    <div className="App">
+      <Balloon/>
+    </div>
+  );
+}
+
+export default App;
---
+++ syngenta-baloon/src/Components/Balloon.jsx
@@ -0,0 +1,60 @@
+
+import './Ballon.css'
+const Balloon = () => {
+  return (
+    <div>
+      <h5>I will complete this assignment with lot's of fun and chill within a time limit</h5>
+      <div className="shadow-box">
+
+{/* empty div start here */} 
+          <div className='main-empty-div'>1
+
+            
+            <div className='sqaure-div'> Empty Div
+                
+
+              
+            </div>
+          
+
+     
+
+
+          </div>
+{/* empty div end here  */}
+
+
+
+
+
+
+
+
+
+
+
+{/* ballon div start here */}
+
+          <div  className='main-ballon-div'>2
+           <div className='Ballon-one' >1</div>
+           <div className='Ballon-two'>2</div>
+           <div className='Ballon-three'>3</div>
+           <div className='Ballon-four'>4</div>
+           <div className='Ballon-five'>5</div>
+          </div>
+
+{/* ballon div end here */}
+
+
+
+
+
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Balloon
